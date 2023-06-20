d6d0b409afc9d492105ec1a53ad36f8743b9dc6f
niranjan <niranjanyadav039@gmail.com>
2023-06-20 09:22:22 +0530

    Intial commit

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ for filepath in filepaths:
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=0, h=8,   )
+    filename = Path(filepath).stem
+    filepath = filename.split("-")
+    i =  filename.split("-")[0]
+    d =  filename.split("-")[1]
+    pdf.cell(w=0, h=8, txt=f"Invoice No. = {i}")
+    pdf.cell(w=8, h=0,txt=f"date= {d}")
+    pdf.output(f"PDFs/{filepath}.pdf")
 
 
